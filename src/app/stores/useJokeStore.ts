626174c3ecc997ffdd769f4936b1c4cc52b1775e
vgont <vgontijo34@gmail.com>
2023-10-19 19:24:58 -0300

    joke setted to ""

--- src/app/stores/useJokeStore.ts
+++ src/app/stores/useJokeStore.ts
@@ -1,15 +1,11 @@
 import { create } from "zustand";
-import useFetchJoke from "../hooks/useFetchJoke";
-
 type Joke = {
   joke: string;
   setJoke: (newJoke: string) => void;
 };
 
-const { joke } = useFetchJoke("https://api.chucknorris.io/jokes/random");
-
 const useJokeStore = create<Joke>((set) => ({
-  joke: joke,
+  joke: "",
   setJoke: (newJoke) => set({ joke: newJoke }),
 }));
 
