8e3bf47ca0bc8f13950162ecfdc2cf687e5574be
vgont <vgontijo34@gmail.com>
2023-10-19 19:07:27 -0300

    change img to next Image

--- src/app/components/ChuckIcon.tsx
+++ src/app/components/ChuckIcon.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 const ChuckIcon: React.FC = () => {
   return (
-    <img
+    <Image
       className="rounded-full w-44 aspect-square object-cover"
       src="https://cdn.dribbble.com/users/1774513/screenshots/3726127/media/ef86424ae75426acfa624dfadcabda14.jpg?resize=400x300&vertical=center"
       alt="Chuck Norris Icon"
