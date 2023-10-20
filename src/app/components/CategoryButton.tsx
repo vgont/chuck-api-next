cc82f1b7cebb7413f8dd9906b51cd24e2b37ad5b
vgont <vgontijo34@gmail.com>
2023-10-20 19:13:49 -0300

    category delay fixed

--- src/app/components/CategoryButton.tsx
+++ src/app/components/CategoryButton.tsx
@@ -1,18 +1,24 @@
 import useCategoryStore from "../stores/useCategoryStore";
 
-const CategoryButton: React.FC<{ category: string; onclick: () => void }> = ({
+interface ICategoryButtonProps {
+  category: string;
+  onclick: () => void;
+}
+
+const CategoryButton: React.FC<ICategoryButtonProps> = ({
   category,
   onclick,
 }) => {
   const { changeCategory } = useCategoryStore();
 
-  const handleClick = () => {
+  const handleCategory = () => {
     changeCategory(category);
     onclick();
   };
+
   return (
     <button
-      onClick={handleClick}
+      onClick={handleCategory}
       className="w-20 md:w-4/12 lg:w-3/12 xl:6/12 h-12 bg-amber-600 hover:bg-amber-700 active:bg-amber-800 text-white text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl font-semibold py-2 px-4 rounded shadow-xl flex justify-center items-center"
     >
       {category}
